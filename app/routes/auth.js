18c99cb11854e2ea013ef3fbd30869461c1b9409
Artur Tomasi <artur@artur>
2016-07-10 02:58:09 -0300

    criando item selector

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,7 +4,7 @@ var passport = require( 'passport' );
 
 module.exports = function ( app )
 {
-    app.post('/login', passport.authenticate('local', { successRedirect: '/#/entity',
+    app.post('/login', passport.authenticate('local', { successRedirect: '/#/posting',
                                                         failureRedirect: '/#/login' } ) );
     
     app.get('/logout', function( req, res )
